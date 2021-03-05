c0b63f36f7812cd0e5489a4edab8b80b80e5512c
Aigul <aigulct@gmail.com>
2021-03-05 12:25:24 -0800

    User can logout

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Aigul Turdugulova on 3/5/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    
+}
